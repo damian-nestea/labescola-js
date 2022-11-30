c6ec46f26af8eada2c636373abd959e66cfa9d96
Damian Martinez <damian@hotmail.cz>
2022-11-30 19:27:50 -0300

    feature - criação dos array contendo os cursos, turmas e estudantes da escola de programação

---
+++ script.js
@@ -0,0 +1,11 @@
+/* 
+// array com os cursos que fazem parte da escola de programação
+const cursos = ["HTML e CSS", "JavaScript", "APIs REST"];
+
+// array com as turmas que estudam na escola de programação
+const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
+
+// array com o nome dos estudantes do curso
+const estudantes = ["João", "Ana", "Cris"];
+ */
+
