add2c7f26b4c24a557aa835de5a774502665a536
Damian Martinez <damian@hotmail.cz>
2022-11-30 19:46:19 -0300

    feature - criação dos cursos inserindo-os em objetos, como também das duas primeiras turmas de Javascript da escola de programação em um array de objetos

--- script.js
+++ script.js
@@ -9,3 +9,110 @@ const turmas = ["HC1", "JS1", "JS2", "REST1", "REST2"];
 const estudantes = ["João", "Ana", "Cris"];
  */
 
+const cursos =[
+  {
+    curso: "HTML e CSS",
+    descricao: "Curso do básico ao avançado de HTML e CSS.",
+    duracao: "1 mês.",
+    valor: 500
+  },
+  {
+    curso: "JavaScript",
+    descricao: "Curso do básico ao avançado de JavaScript.",
+    duracao: "2 meses.",
+    valor: 900
+  },
+  {
+    curso: "APIsRest",
+    descricao: "Curso do básico ao avançado de APIsRest.",
+    duracao: "6 meses.",
+    valor: 2000
+  }
+]; 
+
+const turmas =[
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2022",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Sibyla",
+    curso: "Javascript",
+    inicio: "30/10/2022",
+    termino: "30/12/2022",
+    numeroDeAlunos: 200,
+    periodo: "integral",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  },
+  {
+    turma: "Hipátia",
+    curso: "Javascript",
+    inicio: "30/11/2011",
+    termino: "30/01/2023",
+    numeroDeAlunos: 150,
+    periodo: "noturno",
+    concluida: false
+  }
+];
+
+const estudantes =[
+  {
+    estudante: "",
+    turma: "",
+    curso: "",
+    valor: 0,
+    nParcelas: 0,
+    desconto: true,
+    parcelas: 0
+  }
+]
